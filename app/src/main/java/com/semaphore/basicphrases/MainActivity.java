bb3aff74fa32d65db437330de8f940185e5a9a2e
Marianne Rario <mariannecastillorario27@gmail.com>
2022-04-10 21:30:06 +0800

    add media player that will play the sound of translated phrase

--- app/src/main/java/com/semaphore/basicphrases/MainActivity.java
+++ app/src/main/java/com/semaphore/basicphrases/MainActivity.java
@@ -2,9 +2,26 @@ package com.semaphore.basicphrases;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    //create the playPhrase function
+    public void playPhrase(View view){
+        Button buttonPressed = (Button) view;
+
+        // get the tag of the button that has been pressed
+        Log.i("Button pressed", "Button: " + buttonPressed.getTag().toString());
+        MediaPlayer mPlayer = MediaPlayer.create(this, getResources().getIdentifier(buttonPressed.getTag().toString(), "raw", getPackageName()));
+        mPlayer.start();
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
